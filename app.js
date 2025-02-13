06595a2459c6c9a29fcaac81fd639940d2a6ae2f
Gustavo Borbon <gustavobr351994@gmail.com>
2025-02-13 00:30:29 -0600

    Modificaciones 1

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.prin
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-if(true){
 
+
+let amigos = [];
+
+function agregarAmigo() {
+    let entradaUsuario = document.getElementById("amigo");
+    let nombre = entradaUsuario.value;
+
+    if (nombre == "") {
+        alert("Debe ingresar un nombre."); 
+        return;
+    }
+
+    if(amigos.includes(nombre)){
+        alert(`El nombre ${nombre} ya ha sido agregado.`);
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    document.getElementById("amigo").value = "";
 }
+
