2fcfe8db50d1256db69a1b5094283f406e3b634b
Gustavo Borbon <gustavobr351994@gmail.com>
2025-02-12 23:52:13 -0600

    prueba

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.prin
+
+if(true){
+
+}
